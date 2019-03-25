b2c54bc5cef806901150e4a66b9cf16ffcc130e1
Michelle Lee <lee.mic@husky.neu.edu>
2019-03-24 23:31:23 -0700

    Reactifying the project

---
+++ imports/ui/App.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { Meteor } from "meteor/meteor";
+import { Link } from "react-router-dom";
+import NavBar from "./NavBar.jsx";
+
+import { withTracker } from "meteor/react-meteor-data";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const HomeComponent = () => {
+  return (
+    <div>
+      {Meteor.user() ? <p>sugma</p> : <p>ligma</p>}
+      <p className="text-muted small mb-0">
+        Copyright &copy; DORQ 2019
+      </p>
+    </div>
+  );
+};
+
+const AboutComponent = () => (
+  <div>
+    <p className="text-muted small mb-0">
+      Copyright &copy; DORQ 2019
+    </p>
+  </div>
+);
+
+const NotFoundPage = () => (
+  <div>
+    <h2>Page not found</h2>
+    <div>SUHAS HELP</div>
+  </div>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={HomeComponent} />
+            <Route exact path="/about" component={AboutComponent} />
+            <Route component={NotFoundPage} />
+          </Switch>
+          <br />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default withTracker(() => {
+  return {
+    user: Meteor.user()
+  };
+})(App);
